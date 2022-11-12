9c9ef69447291ad1585b1eaf3e578ef41bccbbb6
Oybek Salomov <oybeksalomov@gmail.com>
2022-11-12 20:15:23 +0500

    Add animations with css and js

--- js/script.js
+++ js/script.js
@@ -2,25 +2,45 @@ window.addEventListener('DOMContentLoaded', () => {
     const choices = document.querySelectorAll('.choice'),
         gameResult = document.querySelector('.gameResult'),
         userChoice = document.querySelector('.user'),
-        computerChoice = document.querySelector('.computer')
+        computerChoice = document.querySelector('.computer'),
+        userResult = document.querySelector('.userResult'),
+        username = document.querySelector('.username'),
+        addInput = document.querySelector('.addInput'),
+        computerResult = document.querySelector('.computerResult'),
+        reset = document.querySelector('.resetBtn')
 
+    /* Birinchi page'dan Username'ni input orqali olib, 2-page'ga qo'yish kerak -----------------------*/
+
+    // get Username from input
     // game function
-    function playGame(user, computer) {
+    let userChangeType = Number(userResult.innerHTML),
+        compChangeType = Number(computerResult.innerHTML)
 
+    function playGame(user, computer) {
         if (user == computer) {
             gameResult.innerHTML = "Tie!"
+           
         } else if (user == 'rock' && computer == 'scissors') {
             gameResult.innerHTML = "You Win!"
+            userResult.innerHTML = 1 + userChangeType++
+            userResult.classList.add('numberAnimation')
         } else if (user == 'scissors' && computer == 'paper') {
             gameResult.innerHTML = "You Win!"
+            userResult.innerHTML = 1 + userChangeType++
+            userResult.classList.add('numberAnimation')
         } else if (user == 'paper' && computer == 'rock') {
             gameResult.innerHTML = "You Win!"
+            userResult.innerHTML = 1 + userChangeType++
+            userResult.classList.add('numberAnimation')
         } else {
             gameResult.innerHTML = "You Lose!"
+            computerResult.innerHTML = 1 + compChangeType++
+            computerResult.classList.add('numberAnimation')
         }
-
-        console.log(user)
-        console.log(computer)
+        setTimeout(() => {
+            userResult.classList.remove('numberAnimation')
+            computerResult.classList.remove('numberAnimation')
+        }, 500)
     }
 
     // Show choices of user and computer function
@@ -28,11 +48,12 @@ window.addEventListener('DOMContentLoaded', () => {
         userChoice.innerHTML = `<img src='img/${user}.png' alt='picture' class='col-6 col-sm-5'>`
         computerChoice.innerHTML = `<img src='img/${computer}.png' alt='picture' class='col-6 col-sm-5'>`
     }
-    
+
     // User choose one
     choices.forEach((item) => {
         item.addEventListener('click', () => {
-
+            userChoice.innerHTML = "<img src='img/rock.png' alt='picture' class='col-6 col-sm-5'>"
+            computerChoice.innerHTML = "<img src='img/rock.png' alt='picture' class='col-6 col-sm-5'>"
             // Computer choose random
             const compRandom = Math.floor(Math.random() * 3)
             let computerChoose
@@ -43,13 +64,24 @@ window.addEventListener('DOMContentLoaded', () => {
             } else if (compRandom == 2) {
                 computerChoose = 'paper'
             }
+            userChoice.classList.add('userAnimation')
+            computerChoice.classList.add('compAnimation')
 
-            playGame(item.name, computerChoose)
-            showChoice(item.name, computerChoose)    
+            setTimeout(() => {
+                userChoice.classList.remove('userAnimation')
+                computerChoice.classList.remove('compAnimation')
+            }, 1000)
+            setTimeout(() => {playGame(item.name, computerChoose)}, 1000)
+            setTimeout(() => {showChoice(item.name, computerChoose)}, 1000)   
         })
     })
     
-    
+    // Reset function
+    reset.addEventListener('click', () => {
+        window.location.reload()
+    })
+
+    // Calculate results function
 
     
 })
